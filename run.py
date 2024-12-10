95a5de8a139567bc596cda9ef8f7a9a929440c33
Steven Clements <sgclements1981@gmail.com>
2024-12-10 13:57:25 +0000

    created get survey data function and added question 1

--- run.py
+++ run.py
@@ -12,8 +12,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('train_stop_survey')
 
-business = SHEET.worksheet('business')
+#business = SHEET.worksheet('business')
+#business_data = business.get_all_values()
 
-business_data = business.get_all_values()
+def get_survey_data():
+    """
+    Get survey data for the user
+    """
 
-print(business_data)
+    print("\nThank you for taking part in our Train Stop survey.\n")
+    print("You will now be asked a few quetsions about your experiance.\n")
+    print("Question 1:")
+    print("Did you use the train today for, business or lesiure?")
+
+    data_str = input("Please answer Business or Lesiure here: ")
+    print(f"The answer you proved was {data_str}")
+
+get_survey_data()
