371b1f4575ae728ac474eb3e7c3ea03c88f88d56
Steven Clements <sgclements1981@gmail.com>
2024-12-10 14:20:02 +0000

    created questions 1 through 5, added input section and reply

--- run.py
+++ run.py
@@ -20,12 +20,24 @@ def get_survey_data():
     Get survey data for the user
     """
 
-    print("\nThank you for taking part in our Train Stop survey.\n")
+    print("\nThank you for taking part in our Train Stop Survey.\n")
     print("You will now be asked a few quetsions about your experiance.\n")
-    print("Question 1:")
     print("Did you use the train today for, business or lesiure?")
 
     data_str = input("Please answer Business or Lesiure here: ")
-    print(f"The answer you proved was {data_str}")
+    print(f"Thank you, you answered {data_str}\n")
+
+    print("Please answer the following questions from 1-5, separated by commas.")
+    print("1 being very dissatisfied, 3 neither and 5 being satisfied.\n")
+    print("Example: 4,3,1,2,5\n")
+
+    print("Question 1: Overall satisfaction of the train.")
+    print("Question 2: Punctuality and relability of service.")
+    print("Question 3: Value for money.")
+    print("Question 4: Level of crowding on the train.")
+    print("Question 5: Frequency of the trains on route.\n")
+
+    data_questions = input("Please provide your answers here: ")
+    print(f"Thank you for your feedback you answered {data_questions}")
 
 get_survey_data()
