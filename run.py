27d15a01b552bc635bbd5a83c63fa405d9410e92
Steven Clements <sgclements1981@gmail.com>
2024-12-11 10:17:41 +0000

    removed inistial question, added survery questions and validated answers from customer survey

--- run.py
+++ run.py
@@ -13,28 +13,52 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('train_stop_survey')
 
 
-def get_opening_survey_data():
+def get_survey_data():
     """
     Get survey data for the user
     """
     
     print("\nThank you for taking part in our Train Stop Survey.\n")
     print("You will now be asked a few questions about your experience.\n")
-    print("First of all we would like to know if you used the train today for business or leisure?")
 
-    opening_question = input("Please answer business or leisure here: ")
-    validate_opening_question(opening_question)
+    while True:
+        print("Please answer the below 6 questions giving them a rating from 1-5.")
+        print("1 very dissatisfied, 2 dissatisfied 3 neither, 4 satisfied, 5 very satisfied.\n")
+        print("Please separate your answers with a commas. Example: 4,3,1,2,5,5\n")
 
-def validate_opening_question(answer):
+        print("Question 1: Overall satisfaction of the train.")
+        print("Question 2: Punctuality and reliability of service.")
+        print("Question 3: Value for money.")
+        print("Question 4: Level of crowding on the train.")
+        print("Question 5: Comfort of seats.")
+        print("Question 6: Frequency of the trains on route.\n")
+
+        survey_str = input("Enter your answers here: ")
+        
+        survey_data = survey_str.split(",")
+        validate_survey(survey_data)
+
+        if validate_survey(survey_data):
+            print("The answers you provided are valid, thank you for your feedback.")
+            break
+        
+    return survey_data
+
+
+def validate_survey(answers):
+    """
+    Check to see if questionnaire data is correct and converted to an int, raise error if string 
+    is incorrect and not between numbers 1-5.
+    """
     try:
-        answers = answer
-        if answer == "business" or "leisure":
-            raise ValueError(f"Please answer either business or leisure.")
+        [int(answer) for answer in answers]
+        if len(answers) != 6:
+            raise ValueError(f"We require all 6 questions to be answered, you answered {len(answers)}")
     except ValueError as e:
-        print(f"Invalid data: {e}")
-        print(answer)
+        print(f"Invalid data: {e}, please try again.\n")    
+        return False
+    return True
 
 
-
-get_opening_survey_data()    
+get_survey_data()    
 
